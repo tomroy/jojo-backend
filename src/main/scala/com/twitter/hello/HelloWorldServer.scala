7bb244427f77f05d005685b8dec175aa00529a72
tom.th_lin <tom.th_lin@htc.com>
2016-09-05 17:00:30 +0800

    UPD. remove pom.xml

--- src/main/scala/com/twitter/hello/HelloWorldServer.scala
+++ src/main/scala/com/twitter/hello/HelloWorldServer.scala
@@ -4,8 +4,14 @@ import com.twitter.finagle.http.filter.Cors
 import com.twitter.finagle.http.filter.Cors.HttpFilter
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.HttpServer
-import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
+import com.twitter.finatra.http.filters.{
+  CommonFilters,
+  LoggingMDCFilter,
+  TraceIdMDCFilter
+}
 import com.twitter.finatra.http.routing.HttpRouter
+
+import scala.sys.ShutdownHookThread
 
 object HelloWorldServerMain extends HelloWorldServer
 
@@ -13,10 +19,21 @@ class HelloWorldServer extends HttpServer {
 
   override def configureHttp(router: HttpRouter) {
     router
-      .filter(new HttpFilter(Cors.UnsafePermissivePolicy), beforeRouting = true)
+      .filter(new HttpFilter(Cors.UnsafePermissivePolicy),
+              beforeRouting = true)
       .filter[LoggingMDCFilter[Request, Response]]
       .filter[TraceIdMDCFilter[Request, Response]]
       .filter[CommonFilters]
       .add[HelloWorldController]
   }
+
+  ShutdownHookThread {
+    info("============================================")
+    info("-------- AccountMainApp stopped.... --------")
+    info("============================================")
+  }
+
+  info("============================================")
+  info(">>>>>>>> AccountMainApp started.... <<<<<<<<")
+  info("============================================")
 }
